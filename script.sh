6e8194ae15c48622d210977794fb007fc28e8cc8
usernamee <86745494+usernameeReal@users.noreply.github.com>
2022-07-09 15:23:27 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,136 @@
+#!/bin/sh
+set -e 
+apt update
+apt -y install software-properties-common curl apt-transport-https ca-certificates gnupg
+LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
+curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | bash
+apt update
+apt -y install php8.1 php8.1-{common,cli,gd,mysql,mbstring,bcmath,xml,fpm,curl,zip} mariadb-server nginx tar unzip git redis-server
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+mkdir -p /var/www/pterodactyl
+cd /var/www/pterodactyl
+curl -Lo panel.tar.gz https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz
+tar -xzvf panel.tar.gz
+chmod -R 755 storage/* bootstrap/cache/
+# Remember to change 'yourPassword' below to be a unique password
+mysql -u root -p << EOF
+CREATE USER 'pterodactyl'@'127.0.0.1' IDENTIFIED BY 'yourPassword';
+CREATE DATABASE panel;
+GRANT ALL PRIVILEGES ON panel.* TO 'pterodactyl'@'127.0.0.1' WITH GRANT OPTION;
+exit
+EOF
+cp .env.example .env
+composer install --no-dev --optimize-autoloader
+
+# Only run the command below if you are installing this Panel for
+# the first time and do not have any Pterodactyl Panel data in the database.
+php artisan key:generate --force
+php artisan p:environment:setup
+php artisan p:environment:database
+php artisan migrate --seed --force
+php artisan p:user:make
+# If using NGINX or Apache (not on CentOS):
+chown -R www-data:www-data /var/www/pterodactyl/*
+
+# If using NGINX on CentOS:
+chown -R nginx:nginx /var/www/pterodactyl/*
+
+# If using Apache on CentOS
+chown -R apache:apache /var/www/pterodactyl/*
+* * * * * php /var/www/pterodactyl/artisan schedule:run >> /dev/null 2>&1
+cat << EOF > /etc/systemd/system/pteroq.service
+# Pterodactyl Queue Worker File
+# ----------------------------------
+
+[Unit]
+Description=Pterodactyl Queue Worker
+After=redis-server.service
+
+[Service]
+# On some systems the user and group might be different.
+# Some systems use `apache` or `nginx` as the user and group.
+User=www-data
+Group=www-data
+Restart=always
+ExecStart=/usr/bin/php /var/www/pterodactyl/artisan queue:work --queue=high,standard,low --sleep=3 --tries=3
+StartLimitInterval=180
+StartLimitBurst=30
+RestartSec=5s
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl enable --now redis-server
+systemctl enable --now pteroq.service
+rm /etc/nginx/sites-enabled/default
+cat << EOF > /etc/nginx/sites-available/pterodactyl.conf
+server_tokens off;
+
+#server {
+#    listen 80;
+#    server_name vm3;
+#    return 301 https://$server_name$request_uri;
+#}
+
+server {
+    listen 80;
+    server_name vm3;
+
+    root /var/www/pterodactyl/public;
+    index index.php;
+
+    access_log /var/log/nginx/pterodactyl.app-access.log;
+    error_log  /var/log/nginx/pterodactyl.app-error.log error;
+
+    # allow larger file uploads and longer script runtimes
+    client_max_body_size 100m;
+    client_body_timeout 120s;
+
+    sendfile off;
+
+    # SSL Configuration - Replace the example <domain> with your domain
+    #ssl_certificate /etc/letsencrypt/live/<domain>/fullchain.pem;
+    #ssl_certificate_key /etc/letsencrypt/live/<domain>/privkey.pem;
+    #ssl_session_cache shared:SSL:10m;
+    #ssl_protocols TLSv1.2 TLSv1.3;
+    #ssl_ciphers "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384";
+    #ssl_prefer_server_ciphers on;
+
+    # See https://hstspreload.org/ before uncommenting the line below.
+    # add_header Strict-Transport-Security "max-age=15768000; preload;";
+    add_header X-Content-Type-Options nosniff;
+    add_header X-XSS-Protection "1; mode=block";
+    add_header X-Robots-Tag none;
+    add_header Content-Security-Policy "frame-ancestors 'self'";
+    add_header X-Frame-Options DENY;
+    add_header Referrer-Policy same-origin;
+
+    location / {
+        try_files $uri $uri/ /index.php?$query_string;
+    }
+
+    location ~ \.php$ {
+        fastcgi_split_path_info ^(.+\.php)(/.+)$;
+        fastcgi_pass unix:/run/php/php8.1-fpm.sock;
+        fastcgi_index index.php;
+        include fastcgi_params;
+        fastcgi_param PHP_VALUE "upload_max_filesize = 100M \n post_max_size=100M";
+        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+        fastcgi_param HTTP_PROXY "";
+        fastcgi_intercept_errors off;
+        fastcgi_buffer_size 16k;
+        fastcgi_buffers 4 16k;
+        fastcgi_connect_timeout 300;
+        fastcgi_send_timeout 300;
+        fastcgi_read_timeout 300;
+        include /etc/nginx/fastcgi_params;
+    }
+
+    location ~ /\.ht {
+        deny all;
+    }
+}
+EOF
+ln -s /etc/nginx/sites-available/pterodactyl.conf /etc/nginx/sites-enabled/pterodactyl.conf
+systemctl restart nginx
+echo "how did we get here?"
